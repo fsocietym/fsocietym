c42174c9384b22e174d2e3c11ee6c724c64b876c
Noor <mohammadnoorayaz@gmail.com>
2024-03-28 19:47:11 +0530

    First commit

---
+++ home/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    rollNo = models.IntegerField()
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=60)
+    
+class RecipeModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    recipe_name = models.TextField(null=True)
+    recipe_description = models.CharField(max_length=1500)
+    recipe_file = models.FileField(upload_to='recipe_images')
---
+++ home/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+
+def home(response):
+    emp = [
+        {'Name': 'Noor', 'Age': 18, 'Salary': 6000},
+        {'Name': 'Rahul', 'Age': 20, 'Salary': 3500},
+        {'Name': 'Saud', 'Age': 19, 'Salary': 4500},
+        {'Name': 'Saurabh', 'Age': 19, 'Salary': 4500}
+    ]
+
+    return render(response, 'index.html', context={'peoples': emp})
+
+
+def about(response):
+    page_title = 'About Us'
+    return render(response, 'about.html', context={'PageTitle':page_title})
+
+
+def contact(response):
+    page_title = 'Contact Us'
+    user_data = ''
+    if response.POST:
+        user_data = response.POST.get('email')
+        print(user_data)
+    
+    return render(response, 'contact.html', context={'PageTitle':page_title, 'mail':user_data}) 
+    
+def recipes(response):
+    page_title = 'Recipes'
+    if response.method == 'POST':
+        recipe_data = response.POST
+        recipe_file = response.FILES.get('recipe_file')
+        RecipeModel.objects.create(recipe_name = recipe_data['recipe_name'],
+                                 recipe_description=recipe_data['recilpe_desc'],
+                                 recipe_file=recipe_file)
+        return redirect('/recipes')
+    
+        
+    
+    return render(response, 'recipes.html' ,context={'PageTitle':page_title})
+
+def explore_recipe(request):
+    recipes_data = RecipeModel.objects.all()
+    page_title = 'Explore Recipe'
+
+    if request.GET.get('search'):
+        recipes_data = RecipeModel.objects.filter(recipe_name__icontains = request.GET.get('search'))
+
+    return render(request, 'explore_recipe.html', context={'recipes':recipes_data, 'PageTitle':page_title,  'data_len': len(recipes_data)})
+
+def delete_recipe(request, id):
+    recipe_data = RecipeModel.objects.filter(id=id)
+    recipe_data.delete()
+    return redirect('/explore_recipe')
+
+def update_recipe(response, id):
+    if response.method == 'POST':
+        post_data = response.POST
+        filtered_data = RecipeModel.objects.get(id=id)        
+        
+        filtered_data.recipe_name = post_data['recipe_name']
+        filtered_data.recipe_description=post_data['recilpe_desc']
+        
+        if response.FILES.get('recipe_file') != None:
+            post_file = response.FILES.get('recipe_file')
+            filtered_data.recipe_file = post_file
+        else:
+            print('old files is set')
+            
+        filtered_data.save()
+        return redirect(f'/update_recipe/{id}')
+            
+    recipe_data = RecipeModel.objects.get(id=id)
+    print(recipe_data.recipe_description)
+    return render(response, 'update_recipe.html', context={'recipes':recipe_data,})
+    
+
+def login(response):
+    return render(response, 'login.html')
+
+def register(response):
+    if response.method == 'POST':
+        # User.first_name = response.POST['first_name']
+        # User.last_name = response.POST['last_name']
+        # User.email = response.POST['email']
+
+        user = User.objects.create(
+                username= response.POST['user_name'],
+                first_name= response.POST['first_name'],
+                last_name= response.POST['last_name'],
+                email= response.POST['email'],                
+        )
+
+
+
+
+        user.set_password(response.POST['password'])
+        user.save()
+        return redirect('/')
+    return render(response, 'register.html')
